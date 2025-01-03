d0d27251676f386df26463e0107a60e78c3dd47c
Jeison Vera <jw.vera.r@gmail.com>
2025-01-03 14:19:46 -0500

    crate success modal for image

--- src/api/image.js
+++ src/api/image.js
@@ -3,5 +3,5 @@ import axios from "./axios";
 export const getImageRequest=(galeryId)=>axios.get(`/image/${galeryId}`)
 //export const getAllImageRequest=()=>axios.get(`/image`)
 export const createImageRequest=(image)=>axios.post('/image',image)
-export const updateImageRequest=(id,image)=>axios.patch(`/image/${id}`,image)
+export const updateImageRequest=(id,image)=>axios.put(`/image/${id}`,image)
 export const deleteImageRequest=(id)=>axios.delete(`/image/${id}`)
